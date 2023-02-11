cd1b9b54ac8fc088f522e7122f9cf5f2877ca8cc
YuliiaKulyk13 <kulyk.yuliia13@gmail.com>
2023-02-11 15:15:27 -0500

    adding some code

--- src/index.js
+++ src/index.js
@@ -1,22 +1,29 @@
 import debounce from 'lodash.debounce';
-import Notiflix from 'notiflix';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import './css/styles.css';
 import fetchCountries from './js/fetchCountries';
 import renderCountries from './js/renderCountries';
 const DEBOUNCE_DELAY = 300;
 
 const input = document.querySelector('#search-box');
+const countryList = document.querySelector('.country-list');
+const countryInfo = document.querySelector('.country-info');
 
 function searchCountries(e) {
   let getCountry = e.target.value.trim();
 
-  fetchCountries(getCountry)
-    .then(data => {
-      console.log(data);
-    })
-    .catch(error => {
-      Notiflix.Notify.failure('Oops, there is no country with that name');
-    });
+  if (getCountry) {
+    fetchCountries(getCountry)
+      .then(dataCountries => {
+        renderCountries(dataCountries);
+      })
+      .catch(error => {
+        Notify.failure('Oops, there is no country with that name');
+      });
+  } else {
+    countryList.innerHTML = '';
+    countryInfo.innerHTML = '';
+  }
 }
 
 input.addEventListener('input', debounce(searchCountries, DEBOUNCE_DELAY));
--- src/js/renderCountries.js
+++ src/js/renderCountries.js
@@ -1,4 +1,43 @@
-export default function renderCountries(data) {
+import { Notify } from 'notiflix';
+
+export default function renderCountries(dataCountries) {
   const countryList = document.querySelector('.country-list');
   const countryInfo = document.querySelector('.country-info');
+
+  let countryItem = [];
+  let quantityCountries = dataCountries.length;
+  let currentCountry = dataCountries[0];
+
+  if (quantityCountries > 10) {
+    Notify.info('Too many matches found. Please enter a more specific name.');
+  } else if (quantityCountries <= 10 && quantityCountries >= 2) {
+    dataCountries.map(country => {
+      countryItem.push(`
+          <li>
+          <img src="${country.flags.svg}" alt='flag' width=40 class="country-flag"/>
+          <span class="country-name">${country.name.official}</span>
+          </li>
+          `);
+    });
+    countryList.innerHTML = countryItem;
+    countryInfo.innerHTML = '';
+  } else {
+    // let country = data[0];
+    // let languages = [];
+    // country.languages.map(language => {
+    //   languages.push(language.name);
+    // }
+    countryInfo.innerHTML = `
+    <img src="${
+      currentCountry.flags.svg
+    }" alt='flag' width=40 class='country-flag'/>
+    <h2 class="country-name">${currentCountry.name.official}</h2>
+    <div>
+    <p>Capital: ${currentCountry.capital}</p>
+    <p>Population:${currentCountry.population}</p>
+    <p>Languages: ${Object.values(currentCountry.languages).join(', ')}</p>
+    </div>
+    `;
+    countryList.innerHTML = '';
+  }
 }
